d7bd2281fbb37a327671b5388361220ea6daad2a
Manheer1018git <139304224+Manheer1018git@users.noreply.github.com>
2023-08-31 15:19:43 +1000

    Update jenkinsfileC.cpp

--- jenkinsfileC.cpp
+++ jenkinsfileC.cpp
@@ -3,7 +3,7 @@ pipeline{
     stages{
         stage('Build'){
             steps{
-                echo "Building the code using Maven"
+                echo "Building the code using Maven."
             }
         }
         stage('Unit and Integration Tests'){
